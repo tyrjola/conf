79b826bc6c415f9c11f6c2637f1c3297f3e34499
tyrjola <timo.yrjola@gmail.com>
2018-01-17 09:53:12 +0200

    scripts: update install script

--- home/scripts/install.sh
+++ home/scripts/install.sh
@@ -5,13 +5,19 @@ ln -s "${HOME}/github/conf/home/.vimrc" "${HOME}/.vimrc"
 ln -s "${HOME}/github/conf/home/.vim" "${HOME}/.vim"
 ln -s "${HOME}/github/conf/home/.astylerc" "${HOME}/.astylerc"
 
+
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+
 sudo apt-get install -yy \
+    vim \
+    python-pip \
+    gitk \
     build-essential \
     cmake \
     python-dev \
     python3-dev \
+    nodejs \
     astyle \
-    npm \
     exuberant-ctags
 
 sudo pip install --upgrade pycodestyle autopep8 docker-compose
