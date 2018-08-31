ff4f4cf93a536047860009082c827da3fd30c2fe
tyrjola <timo.yrjola@gmail.com>
2018-08-31 21:00:24 +0300

    conf: add missing dotfiles

--- home/scripts/install.sh
+++ home/scripts/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ue
 
+ln -s "${HOME}/github/conf/home/.bashrc.user" "${HOME}/.bashrc.user"
 ln -s "${HOME}/github/conf/home/.bash_aliases" "${HOME}/.bash_aliases"
 ln -s "${HOME}/github/conf/home/.vimrc" "${HOME}/.vimrc"
 ln -s "${HOME}/github/conf/home/.vim" "${HOME}/.vim"
+ln -s "${HOME}/github/conf/home/.npmrc" "${HOME}/.npmrc"
 ln -s "${HOME}/github/conf/home/.astylerc" "${HOME}/.astylerc"
 ln -s "${HOME}/github/conf/home/.config/nvim" "${HOME}/.config/nvim"
 ln -s "${HOME}/github/conf/home/.local/share/nvim/site" "${HOME}/.local/share/nvim/site"
@@ -25,7 +27,6 @@ sudo apt-get install -yy \
 pip install --user --upgrade pycodestyle autopep8 docker-compose neovim
 pip3 install --user --upgrade neovim
 
-#mkdir ~/.npm-global
-#npm config set prefix '~/.npm-global'
-#export PATH=~/.npm-global/bin:$PATH
-#npm install -g eslint flow-bin
+mkdir "${HOME}/.npm-global"
+npm config set prefix '~/.npm-global'
+npm install -g eslint flow-bin
